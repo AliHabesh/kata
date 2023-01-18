0cc8416b61464d027bc44fee0148e4c168176b22
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-01-18 09:24:26 +0100

    updated

--- KataDayFour.js
+++ KataDayFour.js
@@ -1,6 +1,7 @@
 
 function replaceVowel(word){
     let lowerCaseWord = word.toLowerCase(); //converts to lower case
+    let resultingWord = "";
     const letterMap = new Map(); //store all the values in a map for fast retrieval
     letterMap.set('a', '1');
     letterMap.set('e', '2');
@@ -10,10 +11,14 @@ function replaceVowel(word){
 
     for(let i = 0; i < word.length; i++){
         if(letterMap.has(lowerCaseWord.charAt(i))){
-            lowerCaseWord = lowerCaseWord.replace(lowerCaseWord.charAt(i), letterMap.get(lowerCaseWord.charAt(i)))
+            resultingWord += letterMap.get(lowerCaseWord.charAt(i));
+        }else{
+            resultingWord += lowerCaseWord.charAt(i);
         }
     }
-   return lowerCaseWord;
+   return resultingWord;
 }
+
+
 
 console.log(replaceVowel("chEmBur")) //ch2mb5r
