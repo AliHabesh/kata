5703d2582b02668abeecec1f815850929725559b
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-01-19 08:51:23 +0100

    updated

--- KataDayFive.js
+++ KataDayFive.js
@@ -1,5 +1,2 @@
-//functional programming solution
-let array = ["1a", "a", "2b", "b"];
-const result = array.filter(word => /[0-9]/.test(word));
-const t = ["1a", "a", "2b", "b"].filter(word => /[0-9]/.test(word));
-console.log(t);
+const result = ["1a", "a", "2b", "b"].filter(word => /[0-9]/.test(word));
+console.log(result);
