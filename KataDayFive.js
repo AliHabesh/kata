4ed6b64c8ab7feccf36fb16d8bd8a79a005829dc
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-01-19 09:41:11 +0100

    removed braces from if statements

--- KataDayFive.js
+++ KataDayFive.js
@@ -1,10 +1,10 @@
 console.log(["this is a test", "test1"].filter(word => {
     for(let i = 0; i < word.length; i++){
-        if(word.charAt(i) == ' '){
+        if(word.charAt(i) == ' ')
             continue;
-        }
-        if(!isNaN(word.charAt(i))){
+        
+        if(!isNaN(word.charAt(i)))
             return word;
-        }
+        
     }
-})); 
+})); //
