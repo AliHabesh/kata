0fd613275c76ecff1add12c4b58eb5aa8b2c4d0e
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-01-23 09:36:25 +0100

    Kata day Six

--- KataDaySix.js
+++ KataDaySix.js
@@ -1,13 +1,13 @@
 const testArray = [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2];
 
 function countBoomerang(input) {
-  let resultArray = [];
+  let amount = 0;
   for (let i = 0; i < input.length; i++) {
     if ((input[i + 1] > i || input[i + 1] < i) && input[i + 2] === input[i]) {
-      resultArray.push([input[i], input[i + 1], input[i + 2]]);
+      amount++;
     }
   }
-  return resultArray;
+  return amount;
 }
 
 console.log(countBoomerang(testArray));
